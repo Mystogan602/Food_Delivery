6f058f113a905dae1749d1e75647d357b74a4a55
Phạm Minh Tiến <phminhtien136@gmail.com>
2024-09-10 17:07:45 +0700

    complete frontend home page

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,9 +3,12 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import {BrowserRouter as Router} from 'react-router-dom'
+import StoreContextProvider from './contexts/StoreContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <Router>
-    <App />
+    <StoreContextProvider>
+      <App />
+    </StoreContextProvider>
   </Router>,
 )
